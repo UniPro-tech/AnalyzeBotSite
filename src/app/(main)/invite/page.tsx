cae31b58e4cbc2b4fa5ede15cc1060c7d65c1a55
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-04-17 01:42:21 +0900

    revalidateを消す

--- src/app/(main)/invite/page.tsx
+++ src/app/(main)/invite/page.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { revalidateTag } from "next/cache";
 import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
@@ -7,7 +6,6 @@ export const metadata: Metadata = {
 };
 
 export default function PostPage() {
-  revalidateTag("discord-guilds", "max");
   redirect(
     "https://discord.com/oauth2/authorize?client_id=1480956404880904232&permissions=4505798919802000&integration_type=0&scope=bot+applications.commands",
   );
