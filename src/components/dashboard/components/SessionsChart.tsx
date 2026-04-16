8007484b592edc3a24a7cdf766fd4346f86ea87f
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-04-16 19:01:58 +0900

    useclientの欠如

--- src/components/dashboard/components/SessionsChart.tsx
+++ src/components/dashboard/components/SessionsChart.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Chip from "@mui/material/Chip";
--- src/components/shared-theme/ColorModeSelect.tsx
+++ src/components/shared-theme/ColorModeSelect.tsx
@@ -1,3 +1,4 @@
+"use client";
 import MenuItem from "@mui/material/MenuItem";
 import Select, { type SelectProps } from "@mui/material/Select";
 import { useColorScheme } from "@mui/material/styles";
